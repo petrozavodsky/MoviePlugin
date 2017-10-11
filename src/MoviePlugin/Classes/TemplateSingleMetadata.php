e9adecf08c3824d70be84286cec73c7abf4abe69
petrozvodsky <petrozavodsky@gmail.com>
2017-10-11 11:16:48 +0300

    Single metadata class  final

--- src/MoviePlugin/Classes/TemplateSingleMetadata.php
+++ src/MoviePlugin/Classes/TemplateSingleMetadata.php
@@ -5,11 +5,16 @@ namespace MoviePlugin\Classes;
 
 
 use MoviePlugin;
+use MoviePlugin\Utils\Assets;
 
 class TemplateSingleMetadata {
 
+	use Assets;
+
 	public function __construct() {
 		add_action( 'MoviePlugin__after', [ $this, 'template_path' ] );
+		$a = $this->addCss( 'SingleMetadata' );
+		d( $a );
 	}
 
 
@@ -21,16 +26,31 @@ class TemplateSingleMetadata {
 	}
 
 	private function show() {
-		$price = MetaBox::$price;
-		$date  = Metabox::$date;
+		$price = get_post_meta( get_the_ID(), MetaBox::$price, true );
+		$date  = get_post_meta( get_the_ID(), Metabox::$date, true );
 		?>
+        <div class="movieplugin__single-metadata-title">
+			<?php _e( 'Additional Information', MoviePlugin::$textdomine ); ?>
+        </div>
+        <div class="MoviePlugin__single-metadata-items">
+            <div class="MoviePlugin__single-metadata-item">
+				<?php the_terms( get_the_ID(), 'countries', '<strong>' . __( 'Country: ', MoviePlugin::$textdomine ) . '</strong>', ',' ); ?>
+            </div>
 
-		<?php the_terms( get_the_ID(), 'countries', 'Country ', ',' ); ?>
-		<?php the_terms( get_the_ID(), 'genres', 'Genre', ',' ); ?>
+            <div class="MoviePlugin__single-metadata-item">
 
-        Release date: <?php echo $date; ?>
-        Price: <?php echo $price; ?>
+				<?php the_terms( get_the_ID(), 'genres', '<strong>' . __( 'Genre: ', MoviePlugin::$textdomine ) . '</strong>', ',' ); ?>
+            </div>
 
+            <div class="MoviePlugin__single-metadata-item">
+				<?php _e( '<strong>Release date: </strong>', MoviePlugin::$textdomine ); ?><?php echo $date; ?>
+            </div>
+
+            <div class="MoviePlugin__single-metadata-item">
+				<?php _e( '<strong>Price: </strong>', MoviePlugin::$textdomine ); ?><?php echo $price; ?>
+            </div>
+
+        </div>
 		<?php
 	}
 
--- templates/single.php
+++ templates/single.php
@@ -41,39 +41,6 @@
 
 
                     <footer class="entry-meta">
-						<?php
-						/* translators: used between list items, there is a space after the comma */
-						$category_list = get_the_category_list( __( ', ', 'unite' ) );
-
-						/* translators: used between list items, there is a space after the comma */
-						$tag_list = get_the_tag_list( '', __( ', ', 'unite' ) );
-
-						if ( ! unite_categorized_blog() ) {
-							// This blog only has 1 category so we just need to worry about tags in the meta text
-							if ( '' != $tag_list ) {
-								$meta_text = '<i class="fa fa-folder-open-o"></i> %2$s. <i class="fa fa-link"></i> <a href="%3$s" rel="bookmark">permalink</a>.';
-							} else {
-								$meta_text = '<i class="fa fa-link"></i> <a href="%3$s" rel="bookmark">permalink</a>.';
-							}
-
-						} else {
-							// But this blog has loads of categories so we should probably display them here
-							if ( '' != $tag_list ) {
-								$meta_text = '<i class="fa fa-folder-open-o"></i> %1$s <i class="fa fa-tags"></i> %2$s. <i class="fa fa-link"></i> <a href="%3$s" rel="bookmark">permalink</a>.';
-							} else {
-								$meta_text = '<i class="fa fa-folder-open-o"></i> %1$s. <i class="fa fa-link"></i> <a href="%3$s" rel="bookmark">permalink</a>.';
-							}
-
-						} // end check for categories on this blog
-
-						printf(
-							$meta_text,
-							$category_list,
-							$tag_list,
-							get_permalink()
-						);
-						?>
-
 						<?php edit_post_link( __( 'Edit', 'unite' ), '<i class="fa fa-pencil-square-o"></i><span class="edit-link">', '</span>' ); ?>
 						<?php unite_setPostViews(get_the_ID()); ?>
                         <hr class="section-divider">
